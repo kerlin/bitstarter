63495911e1392bffb9d4ecb4dcaf4a71fdf9ebc5
Ubuntu <ubuntu@ip-10-116-58-63.ec2.internal>
2013-07-22 18:53:46 +0000

    Initial changes for HW4

--- web.js
+++ web.js
@@ -1,15 +1,26 @@
+// run with node:
+// node web.js
 var express = require('express');
 
 var fs = require('fs');
 
-var app = express.createServer(express.logger());
+// Warning: express.createServer() is deprecated, express
+// applications no longer inherit from http.Server,
+// please use:
+//
+//  var express = require("express");
+//  var app = express();
+//
+//var app = express.createServer(express.logger());
+
+var app = express(express.logger());
 
 app.get('/', function(request, response) {
   var buffer = fs.readFileSync('index.html');
   response.send(buffer.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
